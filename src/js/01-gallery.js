08c22a51244c1ca0ef39beb617cf9feb38c06722
MaksymKonin <skam707@gmail.com>
2023-01-27 13:28:53 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const containerItems = document.querySelector('div.gallery');
+const galleryMarkup = createElementGalleryMarkup(galleryItems);
+containerItems.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createElementGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `</a>
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
